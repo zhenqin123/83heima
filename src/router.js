a36982d35b9c85ed498c35e32c79b2b15405caa1
qin <1044299156@qq.com>
2019-09-19 10:34:58 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home/'
+import Login from './views/login'
 
 Vue.use(Router)
 
@@ -14,6 +15,10 @@ export default new Router({
       path: '/home',
       component: Home
 
+    },
+    {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
